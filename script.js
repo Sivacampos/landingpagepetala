23b5ffe69a41a5ad11745eec023e70096ce90c74
Sivacampos <sivatecnology.io@gmail.com>
2024-03-21 00:20:00 -0300

    landingPagePetala v1.2.0

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ function sendwhatsapp(){
     var email = document.querySelector('.email').value;
     var sevices = document.querySelector('.sevices').value;
 
+   
     var url = "http://wa.me/"+whatsapp+"?text="
+    +"Obrigada pelos dados, breve entrarei em contato!"
     +"*Nome :* " +name+"%0a"
     +"*Telefone :* " +phone+"%0a"
     +"*Email :* " +email+"%0a"
-    +"*Seviços :* " +sevices+"%0a"
-    
+    +"*Seviços :* " +sevices+"%0a";
+    +"Em breve entrarei em contato!"
+
+
     window.open(url, '_blank').focus();
+    
 }
+
+/*let url = "https://api.whatsapp.com/send?phone=5511989126556&text= Bem vindo a Corretora Sousa -->> 
+%0A Qual é o seu nome? %0A " + cliente + ""
+%0A Qual o seu sobrenome ?%0A " + sobrenome +"" 
+%0A Qual o seu e-mail %0A " + email + ""
+%0A Qual é o seu telefone ? %0A " + telefone + ""
+%0A Uma cotação para quantas vidas ? %0A" + exampleFormControlSelect1 + "*/
